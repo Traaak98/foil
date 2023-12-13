c8561fa9b8b00f8e42423fd408ac95466e9b6141
LudoM9 <ludovic.mustiere@ensta-bretagne.org>
2023-12-13 17:18:05 +0100

    Update Launch Package

--- ros2_ws/src/launch_pkg/launch/foil.launch.py
+++ ros2_ws/src/launch_pkg/launch/foil.launch.py
@@ -1,8 +1,10 @@
-import launch
-import launch_ros
-from ament_index_python.packages import get_package_share_directory
 import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     config = os.path.join(
@@ -11,7 +13,7 @@ def generate_launch_description():
         'sbg_device_uart_default.yaml'
     )
 
-    sbg_node = launch_ros.actions.Node(
+    sbg_node = Node(
         package='sbg_driver',
         #	name='sbg_device_1',
         executable = 'sbg_device',
@@ -19,17 +21,31 @@ def generate_launch_description():
         parameters = [config]
     )
 
-    senix_node = launch_ros.actions.Node(
-        package='senix_driver',
-        executable = 'senix_node',
+    foil_state_node = Node(
+        package='foil_state',
+        executable = 'foil_state_node',
+        output = 'screen'
+    )
+
+    foil_objective_node = Node(
+        package='foil_objective',
+        executable = 'foil_objective_node',
+        output = 'screen'
+    )
+
+    foil_consigne_node = Node(
+        package='foil_consigne',
+        executable = 'foil_consigne_node',
         output = 'screen'
     )
 
     rosbag = ExecuteProcess(cmd=['ros2', 'bag', 'record', '-a'],output='screen')
 
 
-    return launch.LaunchDescription([
+    return LaunchDescription([
         sbg_node,
-        senix_node,
+        foil_state_node,
+        foil_objective_node,
+        foil_consigne_node,
         rosbag
     ])
