2183cfa60409592ecad363b2496b9fe128db4e17
gros <louis-nam.gros@ensta-bretagne.org>
2024-02-07 12:29:37 +0100

    launch diviser rosbag

--- ros2_ws/src/launch_pkg/launch/foil.launch.py
+++ ros2_ws/src/launch_pkg/launch/foil.launch.py
@@ -84,15 +84,22 @@ def generate_launch_description():
         package="esp_nuc", executable="esp_nuc_node", output="screen"
     )
 
+    currentFolder = os.path.dirname(os.path.abspath(__file__))
+    rosdirVelodyne = os.path.abspath(os.path.join(currentFolder,os.pardir,os.pardir,os.pardir,os.pardir,os.pardir,'bag_files/velodyne'))
+    os.makedirs(rosdirVelodyne,exist_ok=True)
+    rosdirFoil = os.path.abspath(os.path.join(currentFolder,os.pardir,os.pardir,os.pardir,os.pardir,os.pardir,'bag_files/foil'))
+    os.makedirs(rosdirFoil,exist_ok=True)
+
     rosbag_foil = ExecuteProcess(
         cmd=[
             "ros2",
             "bag",
             "record",
             "-e",
-            "/(sbg/gps_pos|utm_pos|foil_state|foil_consigne|controler_data|foil_objective)",
+            "/(sbg/gps_pos|utm_pos|foil_state|foil_consigne|controler_data|foil_objective|forces_actionneurs|forces_angles)",
         ],
         output="screen",
+        cwd = rosdirFoil,
     )
 
     rosbag_vld = ExecuteProcess(
@@ -104,6 +111,7 @@ def generate_launch_description():
             "/(velodyne_points|velodyne_packets)",
         ],
         output="screen",
+        cwd = rosdirVelodyne,
     )
 
     return launch.LaunchDescription(
@@ -125,6 +133,6 @@ def generate_launch_description():
                 )
             ),
             rosbag_foil,
-            rosbag_vld,
+            rosbag_vld
         ]
     )
