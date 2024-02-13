e490325dbe7e568a0c900abe60db0b44b4a4f0df
LudoM9 <ludovic.mustiere@ensta-bretagne.org>
2024-02-13 16:28:45 +0100

    New consigne Node

--- ros2_ws/src/foil_consigne/src/foil_consigne_node.cpp
+++ ros2_ws/src/foil_consigne/src/foil_consigne_node.cpp
@@ -1,6 +1,5 @@
 #include "foil_consigne/foil_consigne_node.hpp"
 
-
 FoilConsigneNode::FoilConsigneNode() : Node("foil_consigne_node")
 {
     init_parameters();
@@ -16,15 +15,9 @@ FoilConsigneNode::~FoilConsigneNode()
 
 void FoilConsigneNode::init_parameters()
 {
-    Model_inv_ = Model_.inverse();
-    this->declare_parameter<double>("kz_", 0.5);
-    this->declare_parameter<double>("kroll_", 1.0);
-    this->declare_parameter<double>("kpitch_", 5.3); // (180/3.1415)/10
-    this->declare_parameter<double>("kz_proportional_", 0.5);
-    this->declare_parameter<double>("kroll_proportional_", 0.5);
-    this->declare_parameter<double>("kpitch_proportional_", 0.5);
-    this->declare_parameter<double>("kyaw_proportional_", 0.5);
-    //TODO : kspeed_ 
+    this->declare_parameter<double>("kpitch_", 5.3); //* (180/PI)/10 at the beginning of the project
+    this->declare_parameter<double>("kspeed_", 0.2*PI); //TODO: SET THE RIGHT VALUE
+    this->declare_parameter<double>("kroll_", (180/PI)/20); //* (180/PI)/20 at the beginning of the project
 }
 
 void FoilConsigneNode::init_interfaces()
@@ -32,222 +25,7 @@ void FoilConsigneNode::init_interfaces()
     subscription_foil_state_ = this->create_subscription<custom_msg::msg::FoilState>("foil_state", 10, std::bind(&FoilConsigneNode::foil_state_callback, this, std::placeholders::_1));
     subscription_foil_objective_ = this->create_subscription<custom_msg::msg::FoilObjective>("foil_objective", 10, std::bind(&FoilConsigneNode::foil_objective_callback, this, std::placeholders::_1));
     publisher_foil_consigne_ = this->create_publisher<custom_msg::msg::FoilConsigne>("foil_consigne", 10);
-    publisher_forces_actionneurs_ = this->create_publisher<geometry_msgs::msg::Point>("forces_actionneurs", 10);
-    publisher_forces_angles_ = this->create_publisher<geometry_msgs::msg::Point>("forces_angles", 10);
     publisher_parametres_consigne_ = this->create_publisher<custom_msg::msg::ParamConsigne>("parametres_consigne", 10);
-
-}
-
-void FoilConsigneNode::timer_callback()
-{
-    auto msg = custom_msg::msg::FoilConsigne();
-    auto msg_angles = geometry_msgs::msg::Point();
-    auto msg_actionneurs = geometry_msgs::msg::Point();
-    auto msg_parametres = custom_msg::msg::ParamConsigne();
-
-    // double kz_ = 0.5; // TODO: set this parameter
-    // double kroll_ = 0.5; // TODO: set this parameter
-    // double kpitch_ = 0.5; // TODO: set this parameter
-
-    kz_ = this->get_parameter("kz_").as_double();
-    kroll_ = this->get_parameter("kroll_").as_double();
-    kpitch_ = this->get_parameter("kpitch_").as_double();
-
-    msg_parametres.kz = kz_;
-    msg_parametres.kroll = kroll_;
-    msg_parametres.kpitch = kpitch_;
-
-    double g = 9.81;
-
-    // double z_desired = z_objective_*tanh(kz_*speed_);
-    // z_desired = z_; // TODO: TEST PARAMETER. TO BE REMOVED
-    // double z_diff = z_desired - z_;
-
-    // double yaw_desired = atan2(y_objective_ - y_, x_objective_ - x_);
-    // yaw_desired = 0.0; // TODO: TEST PARAMETER. TO BE REMOVED
-    // double yaw_diff = yaw_desired - yaw_;
-
-    // double roll_desired = atan(kroll_*yaw_diff*speed_/g);
-    // roll_desired = 0.0; // TODO: TEST PARAMETER. TO BE REMOVED
-    // double roll_diff = roll_desired - roll_;
-    // double pitch_desired = pitch_objective_*tanh(kpitch_*z_diff);
-    // pitch_desired = 0.0; // TODO: TEST PARAMETER. TO BE REMOVED
-    // double pitch_diff = pitch_desired - pitch_;
-
-    // double kz_proportional = 0.5; // TODO: set this parameter
-    // double kroll_proportional = 0.5; // TODO: set this parameter
-    // double kpitch_proportional = 0.5; // TODO: set this parameter
-
-    kz_proportional_ = this->get_parameter("kz_proportional_").as_double();
-    kroll_proportional_ = this->get_parameter("kroll_proportional_").as_double();
-    kpitch_proportional_ = this->get_parameter("kpitch_proportional_").as_double();
-
-    msg_parametres.kz_proportional = kz_proportional_;
-    msg_parametres.kroll_proportional = kroll_proportional_;
-    msg_parametres.kpitch_proportional = kpitch_proportional_;
-
-
-    double force_u_desired = kz_proportional_*z_diff;
-    double force_roll_desired = kroll_proportional_*roll_diff;
-    double force_pitch_desired = kpitch_proportional_*pitch_diff;
-
-    RCLCPP_INFO(this->get_logger(),
-    "Force u desirée: %f, Force roll désirée: %f, Force pitch désirée: %f",
-    force_u_desired,
-    force_roll_desired,
-    force_pitch_desired);
-
-    msg_angles.x = force_u_desired;
-    msg_angles.y = force_roll_desired;
-    msg_angles.z = force_pitch_desired;
-
-    double force_aileron_left = Model_inv_(0, 0)*force_u_desired + Model_inv_(0, 1)*force_roll_desired + Model_inv_(0, 2)*force_pitch_desired;
-    double force_aileron_right = Model_inv_(1, 0)*force_u_desired + Model_inv_(1, 1)*force_roll_desired + Model_inv_(1, 2)*force_pitch_desired;
-    double force_foil = Model_inv_(2, 0)*force_u_desired + Model_inv_(2, 1)*force_roll_desired + Model_inv_(2, 2)*force_pitch_desired;
-
-    RCLCPP_INFO(this->get_logger(),
-    "Force aileron left: %f, Force aileron right: %f, Force foil: %f",
-    force_aileron_left,
-    force_aileron_right,
-    force_foil);
-
-    msg_actionneurs.x = force_aileron_left;
-    msg_actionneurs.y = force_aileron_right;
-    msg_actionneurs.z = force_foil;
-
-    // On intuite (on a aucune idée de ce que l'on fait mais tracasse, on a qu'un lidar a 4000 balles et une sbg a 2000)
-
-    double alpha1_left_aileron = force_aileron_left;
-    double alpha2_right_aileron = force_aileron_right;
-    double beta_foil = force_foil;
-    double theta_gouvernail = 0.0;
-
-
-    // ########################################################################################### //
-    // ########################################################################################### //
-    //                                                                                             //
-    //                           REGULATION PROPORTIONEL EN CAP                                    //
-    //                                                                                             //
-    // ########################################################################################### //
-    // ########################################################################################### //
-
-    // REGULATION CAP :
-    //    if (yaw_objective_ < 0){
-    //        yaw_objective_+=2*M_PI;
-    //    }
-    //    else{yaw_objective_+= M_PI;}
-    // double yaw_error = sawtooth(yaw_objective_ - yaw_);
-    
-    double yaw_error = yaw_objective_ - yaw_;
-    kyaw_proportional_ = this->get_parameter("kyaw_proportional_").as_double();
-    if (yaw_error >= 0){
-        theta_gouvernail += kyaw_proportional_*yaw_error;
-    }
-    else{
-        theta_gouvernail -= kyaw_proportional_*yaw_error;
-    }
-
-    // ########################################################################################### //
-    // ########################################################################################### //
-    //                                                                                             //
-    //                    REGULATION PROPORTIONEL DU FOIL ARRIERE                                  //
-    //                                                                                             //
-    // ########################################################################################### //
-    // ########################################################################################### //
-
-    pitch_desired = 0.0;
-    pitch_diff = pitch_desired - pitch_;
-    double beta_foil_regul = kpitch_ * pitch_diff;
-    if (beta_foil_regul > 1.){
-        beta_foil_regul = 1.;
-    } else if (beta_foil_regul < -1.){
-        beta_foil_regul = -1.;
-    }
-
-    RCLCPP_INFO(this->get_logger(), "Pitch diff: %f", pitch_diff);
-
-    // ########################################################################################### //
-    // ########################################################################################### //
-    //                                                                                             //
-    //                         REGULATION PROPORTIONEL EN ROLL                                     //
-    //                                                                                             //
-    // ########################################################################################### //
-    // ########################################################################################### //
-
-    // double roll_desired = 0.0;
-    // double roll_diff = roll_desired - roll_;
-    // double roll_foil_regul = kroll_ * roll_diff;
-    // if (roll_foil_regul > 1.){
-    //     roll_foil_regul = 1.;
-    // } else if (roll_foil_regul < -1.){
-    //     roll_foil_regul = -1.;
-    // }
-
-    // alpha1_left_aileron = roll_foil_regul;
-    // alpha2_right_aileron = roll_foil_regul;
-
-    // RCLCPP_INFO(this->get_logger(), "Roll diff: %f", roll_diff);
-
-    // ########################################################################################### //
-    // ########################################################################################### //
-    //                                                                                             //
-    //                         REGULATION PROPORTIONEL EN HAUTEUR                                  //
-    //                                                                                             //
-    // ########################################################################################### //
-    // ########################################################################################### //
-
-    double z_desired = 0.8; // hauteur désirée en mètres
-    z_ = height_est_
-    z_diff = z_desired - z_;
-    
-    // définir kz_ violent  
-    double z_foil_regul = z_desired*tanh(kz_*z_diff);
-    if (z_foil_regul > 1.){
-        z_foil_regul = 1.;
-    } else if (z_foil_regul < 0.){
-        z_foil_regul = 0.;
-    }
-    RCLCPP_INFO(this->get_logger(), "Z diff: %f", z_diff);
-
-    theta_gouvernail = 0.0; //TODO: Remove this parameter
-
-    // Renvoyer un pourcentage d'angle entre -100 et 100 à la liaison série
-    double beta_foil_extrema = 0.6; // TODO: set this parameter$
-    double theta_gouvernail_extrema = 0.6; // TODO: set this parameter
-    double alpha_aileron_extrema = 0.3; // TODO: set this parameter
-    double speed_extrema = 1.0; // TODO: set this parameter
-
-    // TODO: Regarder les angles max et min pour chacun des capteurs.
-
-    // Passage en pourcentage
-    beta_foil = beta_foil/(2*beta_foil_extrema);
-    theta_gouvernail = theta_gouvernail/(2*theta_gouvernail_extrema);
-    alpha1_left_aileron = alpha1_left_aileron/(2*alpha_aileron_extrema);
-    alpha2_right_aileron = alpha2_right_aileron/(2*alpha_aileron_extrema);
-    speed_ = speed_foil_regul/(speed_extrema);
-
-    // TODO: SATURATION DES COMMANDES. NIQUEZ VOUS, ON FLINGUE PAS LES SERVOS CETTE FOIS.
-
-    // Envoyer les données à la liaison série (UART)
-    msg.servo_foil = 100*beta_foil_regul;
-    msg.servo_gouvernail = 100*theta_gouvernail;
-    msg.servo_aileron_left = 100*alpha1_left_aileron;
-    msg.servo_aileron_right = -100*alpha2_right_aileron;
-    msg.thruster = 100*speed_;
-
-    RCLCPP_INFO(
-    this->get_logger(), 
-    "Foil consigne: servo_foil: %f, servo_gouvernail: %f, servo_aileron_left: %f, servo_aileron_right: %f, thruster: %f \n---------------------------------------------------------------\n",
-    msg.servo_foil, 
-    msg.servo_gouvernail, 
-    msg.servo_aileron_left, 
-    msg.servo_aileron_right, 
-    msg.thruster);
-
-    publisher_foil_consigne_->publish(msg);
-    publisher_forces_angles_->publish(msg_angles);
-    publisher_forces_actionneurs_->publish(msg_actionneurs);
-    publisher_parametres_consigne_->publish(msg_parametres);
 }
 
 void FoilConsigneNode::foil_state_callback(const custom_msg::msg::FoilState::SharedPtr msg)
@@ -279,6 +57,75 @@ void FoilConsigneNode::foil_objective_callback(const custom_msg::msg::FoilObject
     this->yaw_objective_ = msg->pose.pose.orientation.z;
 
     this->speed_objective_ = msg->speed;
+}
+
+void FoilConsigneNode::timer_callback() 
+{
+    auto msg_consigne = custom_msg::msg::FoilConsigne();
+
+    read_parameters();
+
+    regulation_pitch();
+
+    regulation_speed();
+
+    regulation_roll();
+
+    // Send data to the publisher
+    msg_consigne.servo_foil = foil_regulation;
+    msg_consigne.thruster = speed_regulation;
+    msg_consigne.servo_aileron_left = roll_regulation;
+    msg_consigne.servo_aileron_right = roll_regulation;
+
+    publisher_foil_consigne_->publish(msg_consigne);
+}
+
+void FoilConsigneNode::read_parameters()
+{
+    auto msg_parameters = custom_msg::msg::ParamConsigne();
+
+    kpitch_ = this->get_parameter("kpitch_").as_double();
+    kspeed_ = this->get_parameter("kspeed_").as_double();
+    kroll_ = this->get_parameter("kroll_").as_double();
+
+    msg_parameters.kpitch = kpitch_;
+    msg_parameters.kspeed = kspeed_;
+    msg_parameters.kroll = kroll_;
+
+    publisher_parametres_consigne_->publish(msg_parameters);
+}
+
+void FoilConsigneNode::regulation_pitch() 
+{
+    foil_regulation = kpitch_ * (pitch_objective_ - pitch_);
+    if (foil_regulation > 1.){
+        foil_regulation = 1.;
+    } else if (foil_regulation < -1.){
+        foil_regulation = -1.;
+    }
+    foil_regulation = 100*foil_regulation;
+}
+
+void FoilConsigneNode::regulation_speed()
+{
+    speed_regulation = tanh(kspeed_ * (z_objective_-z_));
+    if (speed_regulation >= 0.) {
+        speed_regulation = SPEED_OFFSET + (1-0.8)*speed_regulation; //* 0.8 -> plage d'accélération entre 80 et 100%
+    } else {
+        speed_regulation = SPEED_OFFSET + (1-0.9)*speed_regulation; //* 0.9 -> plage de freinage entre 80 et 70%
+    }
+    speed_regulation = 100*speed_regulation;
+}
+
+void FoilConsigneNode::regulation_roll()
+{
+    roll_regulation = kroll_ * (roll_objective_ - roll_);
+    if (roll_regulation > 1.){
+        roll_regulation = 1.;
+    } else if (roll_regulation < -1.){
+        roll_regulation = -1.;
+    }
+    roll_regulation = 100*roll_regulation;
 }
 
 int main(int argc, char * argv[])
