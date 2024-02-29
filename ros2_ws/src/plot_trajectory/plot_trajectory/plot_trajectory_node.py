a82a9fd57dcf25c59d762f36fff8f48147f24979
Apolline <apolline.de_vaulchier@ensta-bretagne.org>
2024-03-01 00:20:55 +0100

    plot trajectoire

---
+++ ros2_ws/src/plot_trajectory/plot_trajectory/plot_trajectory_node.py
@@ -0,0 +1,72 @@
+import rclpy
+from custom_msg.msg import FoilState
+from rclpy.node import Node
+import matplotlib.pyplot as plt
+from math import cos, sin
+
+class Plot(Node):
+
+    def __init__(self):
+        super().__init__("plot")
+        self.x = 0
+        self.y = 0
+        self.psi = 0
+        self.fig, self.ax = plt.subplots()
+        self.init_interface()
+
+    def init_interface(self):
+
+        pointA = [7146300, -5334050]
+        pointB = [7146400, -5334050]
+        pointC = [7146700, -533756]
+        self.ax.scatter(pointA[0], pointA[1], color="red", marker="o", label="Point A", s=100)
+        self.ax.scatter(pointB[0], pointB[1], color="red", marker="o", label="Point B")
+        self.ax.scatter(pointC[0], pointC[1], color="red", marker="o", label="Point C")
+        self.ax.set_xlim(7146050, 7146750)
+        self.ax.set_ylim(-534250, -533800)
+
+        # Souscrire au topic donnant les consignes d'angle des servomoteurs
+        self.foil_state_subscription = self.create_subscription(
+            FoilState, "/foil_state", self.plot_callback, 10
+        )
+
+        self.get_logger().info("Subscribeur et publisher initialisé.")
+
+        # Créer un minuteur pour appeler la fonction time_callback toutes les 2 secondes
+        self.timer = self.create_timer(0.05, self.time_callback)
+
+    def plot_callback(self, msg):
+
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        self.psi = msg.pose.pose.orientation.z
+
+    def time_callback(self):
+        self.ax.scatter(self.x, self.y, color="blue", marker="x")
+        arrow = self.ax.arrow(self.x, self.y, 100 * cos(self.psi), 100 * sin(self.psi))
+        self.get_logger().info("PLotting...")
+        #plt.show()
+        plt.pause(0.5)
+        arrow.remove()
+        self.get_logger().info("PLotting good")
+
+
+    def destroy_node(self):
+        # Fermer la liaison série lors de la fermeture du node
+        self.get_logger().info("Fermeture de la liaison série...")
+        if self.serial_port.is_open:
+            self.serial_port.close()
+
+        super().destroy_node()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    plot = Plot()
+    rclpy.spin(plot)
+    plot.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ros2_ws/src/plot_trajectory/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'plot_trajectory'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='apolline',
+    maintainer_email='apolline.de_vaulchier@ensta-bretagne.org',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'plot_trajectory_node=plot_trajectory.plot_trajectory_node:main'
+        ],
+    },
+)
